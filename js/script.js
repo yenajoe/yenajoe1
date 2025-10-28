ceb509bfb4a4d290fbd8ec960434d7504c2021b2
yenajoe <gusukgi008@naver.com>
2025-10-29 04:45:52 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,11 +4,20 @@ $(document).ready(function () {
   $(".dust").on("click", function () {
     $(this).fadeOut(600);
 
-    $(".totoro").addClass("jump");
+    $(".totoro01").addClass("jump");
+    $("#Right_arm").addClass("arm_move");
+    $("#Right_arm-2").addClass("R_arm_move");
+    $("#Umbrella_behind").addClass("umbrella_behind_move");
+    $("#Umbrella").addClass("umbrella_move");
+    $("#Leaf").addClass("leaf_move");
 
-    $(".totoro").one("animationend", function () {
-      $(".satsuki").addClass("sa-bounce");
-      $(".sign").addClass("si-bounce");
+    $(".totoro01").one("animationend", function () {
+      $(".satsuki01").addClass("sa-bounce");
+      $("#Left_leg").addClass("L_leg_move");
+      $("#Right_leg").addClass("R_leg_move");
+      $("#Sa_Umbrella").addClass("Sa_umbrella_move");
+      $("#Sa_Umbrella_behind").addClass("Sa_umbrella_B_move");
+      $(".sign01").addClass("si-bounce");
     });
   });
 });
